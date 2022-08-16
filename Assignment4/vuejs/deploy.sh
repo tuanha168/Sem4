e8354ee45d234e77afeb73086a72c068b8163634
TuanHa <tuan.ha@tomosia.com>
2022-08-17 01:02:55 +0700

    assignment

--- Assignment4/vuejs/deploy.sh
+++ Assignment4/vuejs/deploy.sh
@@ -1,6 +1,7 @@
 set -e
+sudo systemctl stop nginx.service
 yarn build
 sudo rm -rf /usr/share/nginx/html/
 sudo cp -R ./dist/ /usr/share/nginx/html/
-sudo systemctl restart nginx.service
+sudo systemctl start nginx.service
 echo done
